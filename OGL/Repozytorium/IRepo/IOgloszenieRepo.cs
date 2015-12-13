2b6f26c734f96598078696be7d4c53522fc450b3
Patrycja Borys <patrycja.borys91@gmail.com>
2015-12-13 17:53:53 +0100

    jjj
    jjj

---
+++ OGL/Repozytorium/IRepo/IOgloszenieRepo.cs
@@ -0,0 +1,14 @@
+﻿using Repozytorium.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Repozytorium.IRepo
+{
+    public interface IOgloszenieRepo
+    {
+        IQueryable<Ogloszenie> PobierzOgloszenia();
+    }
+
+}
---
+++ OGL/Repozytorium/Repo/OgloszenieRepo.cs
@@ -0,0 +1,27 @@
+﻿using Repozytorium.IRepo;
+using Repozytorium.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace Repozytorium.Repo
+{
+    public class OgloszenieRepo : IOgloszenieRepo
+    {
+        private readonly IOglContext _db;
+        public OgloszenieRepo(IOglContext db)
+        {
+            _db = db;
+        }
+
+        public IQueryable<Ogloszenie> PobierzOgloszenia()
+        {
+            _db.Database.Log = message => Trace.WriteLine(message);
+            var ogloszenia = _db.Ogloszenia.AsNoTracking();
+            return ogloszenia;
+        }
+
+    }
+}
